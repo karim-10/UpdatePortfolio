57c2bdf599ce4542bead62fa615da89a78155b0a
karim-10 <karremkarrem2017@gmail.com>
2021-02-17 17:14:46 +0100

    tasty-update

--- src/Component/ReactData.js
+++ src/Component/ReactData.js
@@ -1,14 +1,14 @@
 const data5 = [
     {
         type : "React",
-        ProjectName: "Portfolio",
-        url: "https://abdulkarim-barram.netlify.app/",
-        gitUrl:"https://github.com/karim-10/UpdatePortfolio",
+        ProjectName: "Tasty",
+        url: "https://tasty-react.netlify.app/",
+        gitUrl:"https://github.com/karim-10/Tasty-React",
         ReactIcon:"../../img/react.png",
         title:"In diesem Project würde React und React-router-dom benutzt",
-        img:"../../img/portfolio.png" 
+        img:"../../img/tasty-react.png" 
     },
-  /*   {
+    {
         
         ProjectName: "Portfolio",
         url: "https://abdulkarim-barram.netlify.app/",
@@ -16,7 +16,8 @@ const data5 = [
         ReactIcon:"../../img/react.png",
         title:"In diesem Project würde React und React-router-dom benutzt",
         img:"../../img/portfolio.png" 
-    } */
+    },
+
 ]
 
 export default data5;
